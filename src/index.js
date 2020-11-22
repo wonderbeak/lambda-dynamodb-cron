7e84c4f64828b18941290ddc8b3f960473cff2e0
Jaroslavs Rogacs <jaroslavs.rogacs@Sauron.local>
2020-11-22 20:59:28 +0200

    [01] init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB({region: 'eu-central-1'});
+
+exports.handler = async (event) => {
+    const tableName = "timestamps";
+    try {
+        await dynamoDb.putItem({
+            TableName: tableName,
+            Item: {
+                timestamp: {N: Date.now().toString() }
+            }
+        }).promise();
+
+    } catch (error) {
+        throw new Error(`Error in dynamoDB: ${JSON.stringify(error)}`);
+    }
+};
